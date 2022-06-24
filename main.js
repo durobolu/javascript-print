02b3836a9e4de8a8df4e3fe6efeee8a8df652961
durobolu <durobolu@gmail.com>
2022-06-24 02:28:58 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+var myName = "Durotiwon Boluwatife Paul";
+console.log("My name is", myName);
+
+var myHeight = 1.75;
+console.log("My height is", myHeight, "meters");
+
+var myCountry = "Nigeria";
+console.log("My country is", myCountry);
